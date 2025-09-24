34e8768bbb0cab43b95cfb444ef52b8a4864c978
Adelegan-JD <deborahjesudemilade@gmail.com>
2025-09-24 04:09:19 +0100

    Added my task 1 assignment

--- Week_1/adelegan_deborah_task_1/newlines_with_escape_sequence.py
+++ Week_1/adelegan_deborah_task_1/newlines_with_escape_sequence.py
@@ -10,22 +10,7 @@
 # The code below gives my information line by line
 print("TASK 1")
 print("My name is Adelegan Deborah\nI am a graduate of Ladoke Akintola University of Technology(LAUTECH), Ogbomoso.\nI hail from Abeokuta South Local Government\nMy favourite Nigerian food is Pounded Yam and Egusi Soup")
-      
 
-
-
-      
-# TASK 5
-# Write a short 3-line poem about Nigeria and print it using triple quotes("""""")
-
-# Solution
-# This is a short poem on Nigeria
-print("TASK 5")
-print("""      
-      Nigeria is a land filled with milk and honey,
-      And though her leaders rule with the rod of injustice,
-      I believe, and we believe that Nigeria will burgeon
-      """)
       
       
       
---
+++ Week_1/adelegan_deborah_task_1/using_triple_quotes.py
@@ -0,0 +1,11 @@
+# TASK 5
+# Write a short 3-line poem about Nigeria and print it using triple quotes("""""")
+
+# Solution
+# This is a short poem on Nigeria
+print("TASK 5")
+print("""      
+      Nigeria is a land filled with milk and honey,
+      And though her leaders rule with the rod of injustice,
+      I believe, and we believe that Nigeria will burgeon
+      """)
