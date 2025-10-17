77878cfefca0c3d6048cb8bba4a4f0f1dbdfedb0
Adelegan-JD <deborahjesudemilade@gmail.com>
2025-10-17 13:39:37 +0100

    Added backend tasks 1 and 2

---
+++ Week_11/backend_task_1/put.py
@@ -0,0 +1,55 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+data = [
+    {'personal_info' : {
+        'Name' : 'Deborah Adelegan',
+        'Track': 'AI Engineer',
+        'Course': 'Backend Engineering'},
+    'scores' : {
+        'MCQ': '95',
+        'Applied Theory': '90'
+    }
+    },
+    {'personal_info' : {
+        'Name' : 'Deborah Adelegan',
+        'Track': 'AI Engineer',
+        'Course': 'Backend Engineering'},
+    'scores' : {
+        'MCQ': '95',
+        'Applied Theory': '90'
+    }
+    }
+]
+
+class BasicAPI(BaseHTTPRequestHandler):
+    def send_data(self, data, status = 200):
+        self.send_response(status)
+        self.send_header('content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data, indent=2).encode())
+
+    def do_PUT(self):
+        record_id = int(self.path.split('/')[-1])
+        content_size = int(self.headers.get('Content-Length', 0))
+        parsed_data = self.rfile.read(content_size)
+
+        updated_data = json.loads(parsed_data)
+
+        if record_id >= len(data) or record_id < 0:
+            self.send_data({'error': 'Record not found'}, status=404)
+            return
+        
+        else:
+            data[record_id] = updated_data
+            self.send_data({'message': 'Record updated successfully'}, status=200)
+        
+        self.send_data(data)
+
+
+
+def run():
+    HTTPServer(('localhost', 8000), BasicAPI).serve_forever()
+
+
+print('Application is running')
+run()
